f1aa736264f9935a99a90a0e112f5847b26356a6
Chanatip112 <129657878+Chanatip112@users.noreply.github.com>
2025-12-21 19:22:53 +0700

    Add files via upload

--- examples/HanumanMini_BoardTest/HanumanMini_BoardTest.ino
+++ examples/HanumanMini_BoardTest/HanumanMini_BoardTest.ino
@@ -15,34 +15,34 @@ void setup() {
 }
 
 void loop() {
-  if (SW()){
+  if (SW_OK()){
     fd(100);
-    dalay(1000);
+    delay(1000);
     bk(100);
-    dalay(1000);
+    delay(1000);
     sl(100);
-    dalay(1000);
+    delay(1000);
     sr(100);
-    dalay(1000);
+    delay(1000);
     ao();
   }
 
   Serial.print("A0 : ");
-  Serial.print(analog(0));
+  Serial.print(analogRead(0));
   Serial.print("   A1 : ");
-  Serial.print(analog(1));
+  Serial.print(analogRead(1));
   Serial.print("   A2 : ");
-  Serial.print(analog(2));
+  Serial.print(analogRead(2));
   Serial.print("   A3 : ");
-  Serial.print(analog(3));
+  Serial.print(analogRead(3));
   Serial.print("   A4 : ");
-  Serial.print(analog(4));
+  Serial.print(analogRead(4));
   Serial.print("   A5 : ");
-  Serial.print(analog(5));
+  Serial.print(analogRead(5));
   Serial.print("   A6 : ");
-  Serial.print(analog(6));
+  Serial.print(analogRead(6));
   Serial.print("   A7 : ");
-  Serial.print(analog(7));
+  Serial.print(analogRead(7));
 
   Serial.print("   D9 : ");
   Serial.print(digitalRead(9));
