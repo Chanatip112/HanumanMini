0f83d465182f7e4ad84b85008a7c439c00ff0f54
Chanatip112 <129657878+Chanatip112@users.noreply.github.com>
2026-02-05 22:10:21 +0700

    Add files via upload

--- src/Hanuman_MINI_Servo.h
+++ src/Hanuman_MINI_Servo.h
@@ -2,9 +2,9 @@
 /*
  *
  *	File	    	:	  Hanuman_MINI_Servo.h
- *	Release		  : 	v0.3
+ *	Release		  : 	v0.4
  *
- *	Created on	:	  21 Dec 2025
+ *	Created on	:	  5 Feb 2026
  *		  Author	:	  Chanatip112
  *
  */
@@ -18,18 +18,46 @@
 #include "hii.h"
 
 #include<Servo.h>
-Servo myservo;
+Servo servo_pin_9;
+Servo servo_pin_10;
+Servo servo_pin_11;
+Servo servo_pin_12;
+Servo servo_pin_13;
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 void servo(int ch, int deg) {
-  if (ch < 10 || ch > 13){
+  if (ch < 9 || ch > 13){
     return;
   }
-  
-  myservo.attach(ch);
-  deg = constrain(deg, 0, 180);
-  myservo.write(deg);
+
+  switch(ch){
+    case 9:
+      servo_pin_9.attach(9);
+      deg = constrain(deg, 0, 180);
+      servo_pin_9.write(deg);
+      break;
+    case 10:
+      servo_pin_10.attach(10);
+      deg = constrain(deg, 0, 180);
+      servo_pin_10.write(deg);
+      break;
+    case 11:
+      servo_pin_11.attach(11);
+      deg = constrain(deg, 0, 180);
+      servo_pin_11.write(deg);
+      break;
+    case 12:
+      servo_pin_12.attach(12);
+      deg = constrain(deg, 0, 180);
+      servo_pin_12.write(deg);
+      break;
+    case 13:
+      servo_pin_13.attach(13);
+      deg = constrain(deg, 0, 180);
+      servo_pin_13.write(deg);
+      break;
+  }
 }
 
 
